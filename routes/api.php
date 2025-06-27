938374a1b6e9f61daa255ef8142361071f60a08e
zzdep <91667825+zzdep@users.noreply.github.com>
2025-06-27 22:12:12 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,8 @@
     use App\Http\Middleware\MyApiAuth;
     use Illuminate\Support\Facades\Route;
 
+    Route::post('/registration', [ ApiController::class, 'registration' ]);
+
     Route::middleware([ MyApiAuth::class ])->group(function () {
         Route::get('profile', [ ApiController::class, 'profile' ]);
-
-        Route::get('/registration', [ ApiController::class, 'registration' ])
-            ->withoutMiddleware([ MyApiAuth::class ]);
     });
